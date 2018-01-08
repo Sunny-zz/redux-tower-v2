e4c7da330030c4d52a2a836f7860b5512e987d05
Peter Wang <happypeter1983@gmail.com>
2018-01-08 10:17:56 +0800

    store

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import store from '../store'
 
 class Home extends Component {
   render () {
+    console.log('store', store.getState())
     return (
       <div>
         Home
