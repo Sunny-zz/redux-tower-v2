be0b5053e7534ab155f4915219b535f912085203
Peter Wang <happypeter1983@gmail.com>
2018-01-08 10:58:21 +0800

    show poster

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 class Home extends Component {
   render () {
     const { courses } = this.props
     console.log(courses)
+    const list = courses.map(
+      t => (
+        <CourseCard key={t.id}>
+          <img alt='poster' src={t.poster} />
+        </CourseCard>
+      )
+    )
     return (
-      <div>
-        Home
-      </div>
+      <Wrap>
+        {list}
+      </Wrap>
     )
   }
 }
 
 export default Home
+
+const Wrap = styled.div`
+  display: flex;
+  width: 700px;
+  margin: 20px auto;
+  justify-content: space-between;
+`
+const CourseCard = styled.div`
+  width: 300px;
+  img {
+    width: 100%;
+    display: block;
+  }
+`
