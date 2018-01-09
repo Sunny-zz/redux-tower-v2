c42f498ad1c39604fd3ccd54974918859f18d49c
Peter Wang <happypeter1983@gmail.com>
2018-01-09 16:46:55 +0800

    combineReducers

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -6,7 +6,7 @@ import { like, goComment } from '../actions'
 const HomeContainer = props => <Home {...props} />
 
 const mapStateToProps = state =>({
-  courses: state
+  courses: state.courses
 })
 // mapDispatchToProps 的简写形式
 export default connect(mapStateToProps, {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,35 +1,8 @@
-const initialState = [
-  {
-    id: '1',
-    poster: 'http://o86bpj665.bkt.clouddn.com/posters/meteor-react-bird.png',
-    title: '课程1',
-    likes: 2
-  }, {
-    id: '2',
-    poster: 'http://o86bpj665.bkt.clouddn.com/posters/meteor-react-bird.png',
-    title: '课程2',
-    likes: 3
-  }
-]
+import { combineReducers } from 'redux'
+import courses from './courses'
+import comments from './comments'
 
-const rootReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case 'LIKE':
-      const { courseId } = action
-      return state.map(
-        t => {
-          if (t.id === courseId) {
-            return {
-              ...t,
-              likes: t.likes + 1
-            }
-          }
-          return t 
-        }
-      )
-    default:
-      return state
-  }
-}
-
-export default rootReducer
+export default combineReducers({
+  comments,
+  courses
+})
