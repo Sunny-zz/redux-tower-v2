899d1a7b7e6c507e32a214ac55eadb37aebf8c7f
Peter Wang <happypeter1983@gmail.com>
2018-01-10 09:19:54 +0800

    course layout

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 class Course extends Component {
   render () {
     return (
-      <div>
-        Course
-      </div>
+      <Wrap>
+        <Upper>
+          Upper
+        </Upper>
+        <Lower>
+          Lower
+        </Lower>
+      </Wrap>
     )
   }
 }
 
 export default Course
+
+const Wrap = styled.div`
+`
+
+const Upper = styled.div`
+  min-height: 30vh;
+  background: #00bcd4;
+`
+
+const Lower = styled.div`
+  min-height: 60vh;
+  background-color: #eeeeee;
+`
